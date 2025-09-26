83a4ce83df4aff0aa715277a5f1378053117b7dd
Damtab83 <damtab@hotmail.com>
2025-09-26 14:01:18 +0200

    modify app.routes.ts for verify prod routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,8 +5,8 @@ import { CGV } from './components/cgv/cgv';
 import { MentionsLegales } from './components/mentions-legales/mentions-legales';
 
 export const routes: Routes = [
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: 'home', component: Home },
+  { path: '', component: Home },
+  { path: 'home', redirectTo: '', pathMatch: 'full' },
   { path: 'cgv', component: CGV },
   { path: 'mentions-legales', component: MentionsLegales },
   { path: '**', component: NotFound },
