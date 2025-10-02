109e7aa442fb3de69ef3c6aec2c5c7d88c862add
Damtab83 <damtab@hotmail.com>
2025-10-02 14:29:42 +0200

    Created signin page

--- src/app/components/signin/signin.ts
+++ src/app/components/signin/signin.ts
@@ -1,11 +1,73 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import {
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { merge } from 'rxjs';
 
 @Component({
   selector: 'jo2024-signin',
-  imports: [],
+  standalone: true,
+  imports: [
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+    MatIconModule,
+  ],
+  changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './signin.html',
-  styleUrl: './signin.css'
+  styleUrl: './signin.css',
 })
 export class Signin {
+  form = new FormGroup({
+    firstname: new FormControl('', [Validators.required]),
+    lastname: new FormControl('', [Validators.required]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required]),
+  });
 
+  errorMessage = signal('');
+
+  constructor() {
+    const emailControl = this.form.get('email')!;
+    merge(emailControl.statusChanges, emailControl.valueChanges)
+      .pipe(takeUntilDestroyed())
+      .subscribe(() => this.updateErrorMessage());
+  }
+
+  updateErrorMessage() {
+    const email = this.form.get('email');
+    if (email?.hasError('required')) {
+      this.errorMessage.set('You must enter a value');
+    } else if (email?.hasError('email')) {
+      this.errorMessage.set('Not a valid email');
+    } else {
+      this.errorMessage.set('');
+    }
+  }
+
+  hide = signal(true);
+  clickEvent(event: MouseEvent) {
+    this.hide.set(!this.hide());
+    event.stopPropagation();
+  }
+
+  onSubmit() {
+    if (this.form.valid) {
+      console.log('✅ Connexion établie :', this.form.value);
+    } else {
+      console.warn('❌ Connexion invalide');
+      this.form.markAllAsTouched();
+    }
+  }
 }
